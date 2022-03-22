b4b7b1cb1494581f4fc1d46a6a59bb84de1b294e
Minhaj Azim Abdullah <78309013+minhaj2OOO@users.noreply.github.com>
2022-03-22 16:49:18 +0600

    Add files via upload

---
+++ src/context/GlobalContext.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const GlobalContext = React.createContext({
+  monthIndex: 0,
+  setMonthIndex: (index) => {},
+  daySelected: null,
+  setDaySelected: (day) => {},
+  showEventModal: false,
+  setShowEventModal: () => {},
+  dispatchCalEvent: ({ type, payload }) => {},
+  savedEvents: [],
+  selectedEvent: null,
+  setSelectedEvent: () => {},
+  filteredEvents: [],
+});
+
+export default GlobalContext;
